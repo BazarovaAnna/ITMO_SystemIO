d3e18ce11a2ffbeadf447b143a451fc2bd52a204
Anna Bazarova <243845@niuitmo.com>
2021-02-26 09:39:21 +0000

    final report

---
+++ lab1/.lab1.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/mint/Desktop/sysio/lab1/lab1.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/mint/Desktop/sysio/lab1/lab1.ko /home/mint/Desktop/sysio/lab1/lab1.o /home/mint/Desktop/sysio/lab1/lab1.mod.o ;  true
